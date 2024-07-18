f5b5427daaeab268fcd53dd45b9bc20215ba5e5b
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2024-07-18 13:04:20 -0300

    add messageAlegria2

--- src/consts/messageAlegria.ts
+++ src/consts/messageAlegria.ts
@@ -6,7 +6,7 @@ export const messageAlegria = [
     title: "Paulo e Silas na Prisão",
     verse: [
       "‘Por volta da meia-noite, Paulo e Silas estavam orando e cantando hinos a Deus, e os outros presos os ouviam. De repente, houve um terremoto tão violento que os alicerces da prisão foram abalados. Imediatamente, todas as portas se abriram, e as correntes de todos se soltaram.’",
-      "(Atos 16: 25-26 )",
+      "(Atos 16: 25-26)",
     ],
     text: [
       "Recomendo que leiam Atos 16: 16-40.",
@@ -30,6 +30,28 @@ export const messageAlegria = [
       "Que grande exemplo desses dois! Mantiveram o ânimo no Senhor; as correntes caíram e ainda pregaram a preciosa Palavra do Senhor. Mantenham-se na esperança e alegria no Senhor; o resto Ele cuida. Orem e Louvem, pois Ele é digno de toda adoração.",
 
       "‘Deus é o nosso refúgio e fortaleza, auxílio sempre presente na adversidade. Por isso não temeremos, ainda que a terra trema e os montes afundem no coração do mar, ainda que estrondem as suas águas turbulentas e os montes sejam sacudidos pela sua fúria.’ - Salmos 46:1-3",
+    ],
+  },
+  {
+    id: "al2",
+    author: "Danyllo Aniceto",
+    instagram: "danylloaniceto",
+    title: "Louve na Presença Senhor com Alegria",
+    verse: [
+      "‘Adorem o SENHOR com alegria e venham cantando até a sua presença.’",
+      "(Salmos 100: 2)",
+    ],
+    text: [
+      "Estar na presença do Senhor é o maior privilégio que alguém pode ter. É tão bom estar perto Dele, sabendo que Ele realmente nos ama. Sua proteção nos traz paz e podemos engrandecer o Seu nome com razão, pois Ele é digno de todo o louvor. Podemos confiar Nele e, com todo o nosso coração, clamar a Ele com nossas orações e louvores. Jesus nos deu razão para viver uma vida em abundância, permitindo-nos ter esperança de viver eternamente ao Seu lado, onde não haverá mais morte, nem choro e nem dor.",
+      "Deus é poderoso, capaz de realizar todas as coisas. Podemos lançar sobre Ele todas as nossas preocupações, pois Ele cuida de tudo. É tão bom lembrar de Seus feitos; Ele nunca falhou, sempre operou grandes vitórias. Quando os israelitas pensaram que seriam capturados pelos egípcios, Deus partiu o mar em dois para que Israel pudesse atravessar e fugir dos que os perseguiam. Quando todos os israelitas passaram, o mar se fechou e todos os egípcios foram engolidos pelas águas. José foi jogado no poço pelos seus irmãos, mas Deus o fez governador do Egito. Daniel foi lançado na cova dos leões, mas Deus fechou a boca dos leões. Sadraque, Mesaque e Abede-Nego foram lançados na fornalha, mas Deus passeava com eles. Os 450 profetas de Baal não conseguiram fazer com que seu deus fizesse chover fogo, mas Elias orou e o fogo caiu do céu. Quando todos nós estávamos condenados à morte pelos nossos pecados, Jesus se fez carne e morreu por nós. Grandes são os motivos para louvar e se alegrar por quem o Senhor é, pelo que Ele fez, tem feito e fará.",
+      "Em 2 Samuel 6, encontramos um episódio na vida de Davi, onde ele deseja trazer a arca da aliança para Jerusalém, o que Saul não havia feito. Davi desejava que a presença do Senhor estivesse com ele. Na primeira tentativa de trazer a arca, não deu muito certo, pois estavam fazendo da maneira errada. Havia um jeito certo de se comportar, e Uzá, ao tocar a arca com a mão, morreu, pois não podia tocá-la. Então, Davi deixou a arca por três meses na casa de Obede-Edom. Durante esses meses, o Senhor abençoou muito Obede-Edom e sua família. Após esse tempo, Davi finalmente trouxe a arca da aliança para Jerusalém da maneira correta. Enquanto ele trazia a arca, todos gritavam de alegria, tocavam as trombetas e Davi dançou com todas as suas forças em louvor a Deus.",
+      "Mical, uma das esposas de Davi, observava Davi dançando no meio do povo em louvor a Deus, e ela sentiu profundo desprezo por ele. Após Davi oferecer sacrifícios ao Senhor e abençoar o povo no nome do Senhor, todos foram embora, e então Mical foi até ele reclamar do que ele havia feito, parecendo um homem vulgar. Davi respondeu que estava dançando em louvor ao Senhor e ainda continuaria a dançar e se humilhar ainda mais diante da presença de Deus.",
+      "Davi sentia muita alegria ao trazer a arca para Jerusalém. Ele sabia que o que estava fazendo era algo muito bom; Davi amava a Deus e sempre O queria por perto. Ele sentiu tanto entusiasmo ao trazer a presença de Deus junto com ele. Assim, devemos nós estarmos alegres pelo Espírito Santo fazer morada no nosso coração, Aquele que nos consola e nos traz ânimo até mesmo em momentos de aflição. Repousamos em Seus braços e podemos dormir tranquilos, pois estamos debaixo de Sua proteção. Por isso, sempre louvemos ao Senhor, que não paremos de cantar e dançar de alegria na presença Dele. Ele nos traz verdadeiros motivos para nos tornar alegres, então que não tenhamos vergonha e possamos demonstrar toda nossa devoção com fervor ao Senhor.",
+      "Deus realmente só traz coisas boas, porque Ele é bom. Tudo que há Nele é bom, tudo é perfeito, Ele nos satisfaz. Nosso Deus é um Deus abençoador, que nos traz grandes maravilhas, assim como Ele abençoou Obede-Edom e sua casa, assim Ele pode ser conosco. Não perca a esperança nisso, continue louvando e se alegrando no que Ele é. Deus vai abençoar nossa casa, Ele tem poder para isso. Louve a Ele, aleluia!",
+      "‘Bendito seja o Deus e Pai do nosso Senhor Jesus Cristo, que nos abençoou com todas as bênçãos espirituais nas regiões celestiais em Cristo.’ – Efésios 1: 3",
+      "Que nosso coração sempre possa estar grato ao Senhor. Assim, nos livramos de toda amargura que nos torna cegos e julgadores, que nos impede de reconhecer as bênçãos e se alegrar no Senhor. Mical apenas via seu marido dançando no meio do povo de forma vergonhosa, ao invés de se alegrar pelo mesmo motivo que Davi se alegrava, ela ficou descontente e deixou que seu coração não permitisse se entregar no louvor a Deus. Ela murmurou contra um ato de adoração ao Senhor, e isso é muito sério. Que nossa boca nunca reclame disso, que possamos excluir qualquer pensamento como esse de nossas mentes, que mantenhamos nossas bocas fechadas se não tivermos algo bom para falar.",
+      "‘Nenhuma palavra torpe saia da boca de vocês, mas apenas a que for útil para edificar os outros, conforme a necessidade, para que transmita graça aos que a ouvem.’ – Efésios 4: 29",
+      "Pois só há verdadeira alegria onde a presença do Senhor permanece. Que possamos aprender com a história do filho pródigo, que quando ele perdeu tudo lá fora no mundo, percebeu que era melhor ter permanecido junto ao pai, onde ele era bem cuidado e tratado com amor, e não com interesse. Então, não desista do caminho do Senhor e continue louvando e se alegrando na presença Dele. Não se envergonhe, tenha fervor e seja exemplo para que outros também glorifiquem a Deus. Que possamos sempre dançar em Sua presença sem medo dos olhares de outros, pois Aquele que nos observa se agrada de nossa alegria a Ele. Aleluia! Todo louvor seja dado a Jesus Cristo!",
     ],
   },
 ];
