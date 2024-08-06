7d9e08ac2553a86c73980c2d928983002772a70e
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2024-08-06 16:51:40 -0300

    add pa2

--- src/consts/messagePaz.ts
+++ src/consts/messagePaz.ts
@@ -32,4 +32,27 @@ export const messagePaz = [
       "Uma pessoa cheia de paz transmite luz para aqueles que ainda não enxergam. Essa luz é Cristo. Por isso, é bom termos paz, não apenas por nós mesmos, mas também pelo próximo que nos observa e reconhece o agir de Deus em nossa vida, e a nossa devoção e confiança no Senhor Jesus. O que Cristo fez por nós é o suficiente para que nossa vida trilhe um caminho sem anseios, mas de paz.",
     ],
   },
+  {
+    id: "pa2",
+    author: "Danyllo Aniceto",
+    instagram: "danylloaniceto",
+    title: "A Paz em Meio às Tempestades",
+    verse: [
+      "'Tu guardarás em perfeita paz aquele cujo propósito está firme, porque ele confia em ti.'",
+      "(Isaías 26:3)",
+    ],
+    text: [
+      "Quantas vezes já nos deparamos com momentos de aflição, enfrentando problemas que parecem irreversíveis? Nesses momentos, nossos próprios olhos não conseguem enxergar uma solução, o que pode nos tornar ansiosos e dificultar nossa fé em um Deus que realiza milagres e pode fazer o impossível. Quando confiamos no Senhor, depositamos toda nossa confiança Nele, certos de que estamos seguros e de que tudo, no final, dará certo. Estar ao lado de Jesus é estar ao lado da vitória. As circunstâncias difíceis que enfrentamos não definem nosso destino final. Problemas não devem ser motivo para desistir da fé ou se amedrontar, mas sim para dar um passo de confiança no Senhor e ter esperança de que tudo pode ser resolvido em Suas mãos.",
+      "Quando Pedro foi preso por Herodes, ele poderia ter se desesperado, passando a noite inteira em claro pensando na sua possível morte, pois o apostolo Tiago já havia sido morto, então ele também seria. Mas, ao invés disso, Pedro deitou-se, dormiu e descansou nos braços do Senhor, pois estava com o Senhor. Independentemente do que acontecesse, a paz que o Senhor nos dá nos deixa seguros de que, seja o que tiver que acontecer se estamos com o Senhor já estamos no lucro.",
+      "‘Na noite antes do dia em que Herodes ia apresentá-lo ao povo, Pedro estava dormindo, preso com duas correntes, entre dois soldados; e havia guardas de vigia no portão da cadeia’ – Atos 12: 6",
+      "‘Porque para mim o viver é Cristo, e o morrer é lucro.’ – Filipenses 1: 21",
+      "Acredito que Pedro aprendeu essa lição quando ele e os discípulos estavam no meio de uma tempestade. Todos se desesperavam, amedrontados pelo barco balançando e os trovões. Enquanto isso, Jesus dormia. Os discípulos se perguntaram como Jesus podia dormir no meio de uma tempestade tão assustadora. Com Sua sabedoria e paz, Jesus acalmou a tempestade, mostrando Seu poder.",
+      "‘Ele se levantou, repreendeu o vento e disse ao lago: ― Aquiete‑se! Acalme‑se! O vento se aquietou, e tudo ficou calmo.’ – Marcos 4: 39",
+      "Assim, quando estamos com Jesus, Ele pode acalmar qualquer situação. Essa é a lição que devemos aprender: firmar-nos na Palavra e ter coragem e segurança de que o Senhor cuida de nós e provê tudo o que precisamos.",
+      "Em momentos de dificuldade, não precisamos nos desesperar ou deixar que a ansiedade tome conta de nós. Deus pode trazer provisão, pois Ele pode tudo. Assim como Pedro foi libertado da prisão, nossos problemas podem ser resolvidos. Acalme-se e confie, pois quando andamos com o Senhor, temos a certeza de que Ele cuida de nós. Estar em Seus braços é a maior segurança que podemos ter. Tome cuidado para que seus questionamentos não atrapalhem sua fé, Deus não age como nós agimos, os pensamentos Dele são muito maiores do que os nossos, tenha compreensão que o que Ele pode fazer por nós está além do que os nossos olhos viram e nossos ouvidos ouviram.",
+      "‘Deixo com vocês a paz. É a minha paz que eu lhes dou; não lhes dou a paz como o mundo a dá. Não fiquem aflitos, nem tenham medo.’ – João 14: 27",
+      "Nosso coração pode, às vezes, nos enganar, mas se queremos ver a verdade, devemos voltar-nos para a imagem de Deus e lembrar de quem Ele é. Meditando em Sua Palavra, encontraremos paz e enfrentaremos qualquer coisa, sabendo que tudo será resolvido pelas mãos do Senhor.  Ele é o Senhor de nossas vidas e cuida de nós. E mesmo que uma situação não saia como esperado continue glorificando a Ele e crendo, pois quando vemos a história de pessoas que andaram com Jesus vemos que a carreira deles terminam com uma morte trágica, como o próprio Pedro sendo crucificado de cabeça pra baixo, mas isso significa perder, não somos derrotados e nem amedrontados enquanto a presença de Jesus estiver sobre nós.",
+      "Que possamos ter a paz de Deus em meio às tempestades e prisões, crendo que Ele tem poder para nos livrar de todo o mal.",
+    ],
+  },
 ];
